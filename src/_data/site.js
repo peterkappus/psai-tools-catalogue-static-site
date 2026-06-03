a65c9e2d7ea541454c928a46648da6aec97a7953
Peter Kappus <62189+peterkappus@users.noreply.github.com>
2026-06-03 13:04:58 +0100

    enable GA and cookie banner, add info to about page.

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
   serviceName: "Public sector AI tools catalogue",
   serviceUrl: "/",
-  ga4MeasurementId: null,
+  ga4MeasurementId: "G-HNWWW5QV6C",
   pageTitleBase: "Public Sector AI tools catalogue",
   branding: {
     // Set to true only if you're allowed to use GOV.UK branding assets.
